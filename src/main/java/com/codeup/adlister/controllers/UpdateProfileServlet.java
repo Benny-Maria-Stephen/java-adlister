48638a1d0fabff24fbdf018228c42e8015e0be19
Stephen Guedea <guedea.stephen@gmail.com>
2018-12-18 15:54:09 -0600

    Completed UpdatedProfile

--- src/main/java/com/codeup/adlister/controllers/UpdateProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/UpdateProfileServlet.java
@@ -11,29 +11,41 @@ import java.io.IOException;
 @WebServlet(name = "controllers.UpdateProfileServlet", urlPatterns = "/update-profile")
 public class UpdateProfileServlet extends HttpServlet {
     protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        String username = request.getParameter("username");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
         String passwordConfirmation = request.getParameter("confirm_password");
+        Long id = (Long)request.getSession().getAttribute("id");
 //
         request.getSession();
 //
 //        // validate input
 //        // is email empty, password,
+        boolean inputHasErrors = username.isEmpty()
+                || email.isEmpty()
+                || password.isEmpty()
+                || (! password.equals(passwordConfirmation));
+
+        if (inputHasErrors) {
+            response.sendRedirect("/update-profile");
+            return;
+        }
+        //        // create and save a new user
+        User user = new User(id, username, email, password);
+        DaoFactory.getUsersDao().updateUser(user);
+        response.sendRedirect("/");
 //
 //        request.getRequestDispatcher("/WEB-INF/partials/profile.jsp").forward(request, response);
     }
 
     protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-
+        request.getRequestDispatcher("/WEB-INF/updateProfile.jsp").forward(request, response);
 //        if (inputHasErrors) {
 //            response.sendRedirect("/WEB-INF/updateProfile.jsp");
 //            return;
 //        }
 //
-//        // create and save a new user
-//        User user = new User(username, email, password);
-//        DaoFactory.getUsersDao().insert(user);
-//        response.sendRedirect("/login");
+
 
     }
 }
--- src/main/webapp/WEB-INF/updateProfile.jsp
+++ src/main/webapp/WEB-INF/updateProfile.jsp
@@ -12,6 +12,11 @@
     <form action="/update-profile" method="post">
 
         <div class="form-group">
+            <label for="username">Username</label>
+            <input id="username" name="username" class="form-control" type="text">
+        </div>
+
+        <div class="form-group">
             <label for="email">Email</label>
             <input id="email" name="email" class="form-control" type="text">
         </div>
