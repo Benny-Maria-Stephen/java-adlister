f866c105d703ddc594e0ece6550d2b7e1dea186a
benny-fields3rd <benny.fields3rd@gmail.com>
2018-12-19 15:18:20 -0600

    working delete user

--- src/main/java/com/codeup/adlister/controllers/DeleteUserServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DeleteUserServlet.java
@@ -1,6 +1,8 @@
 package com.codeup.adlister.controllers;
+
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,10 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "controllers.UpdateProfileServlet", urlPatterns = "/update-profile")
-public class UpdateProfileServlet extends HttpServlet {
+@WebServlet(name = "DeleteUserServlet", urlPatterns = "/update-profile")
+public class DeleteUserServlet extends HttpServlet {
     protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-        String id = request.getParameter("id");
+//        String id = request.getParameter("id");
         String username = request.getParameter("username");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
@@ -19,20 +21,17 @@ public class UpdateProfileServlet extends HttpServlet {
         User user = (User) request.getSession().getAttribute("user");
         request.getSession();
 
-        // Is this needed and does it need to be modified
         boolean inputHasErrors = username.isEmpty()
                 || email.isEmpty()
                 || password.isEmpty()
                 || (!password.equals(passwordConfirmation));
-        // This too???
         if (inputHasErrors) {
             response.sendRedirect("/update-profile");
             return;
         }
-
-        // update profile
-        DaoFactory.getUsersDao().updateUser(new User(user.getId(), username, email, password));
-        response.sendRedirect("/profile");
+        // delete profile
+        DaoFactory.getUsersDao().deleteUser(user.getId());
+        response.sendRedirect("/login");
     }
 
     protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
